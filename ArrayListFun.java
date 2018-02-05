dd736c4b74c674c8571050676cd939e61f21c4c2
DrDarkfire <2017BrandonH@gmail.com>
2018-02-05 10:48:54 -0500

    Add files via upload

---
+++ ArrayListFun.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class ArrayListFun {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		al.add(1); // add element to the end of the list
+		al.add(2);
+		al.add(3);
+	}
+
+}
