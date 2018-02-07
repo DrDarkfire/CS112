908a995de018b2a64f60a48dbee50095b610e3d7
kmurdough <36051815+kmurdough@users.noreply.github.com>
2018-02-07 10:02:02 -0500

    Lotta if statements

--- ToadsAndFrogsSolver.java
+++ ToadsAndFrogsSolver.java
@@ -14,17 +14,58 @@ public class ToadsAndFrogsSolver {
 		in.close();
 	}
 
-	public static char getWinner(char[] board, char currentPlayer) {
-		// TODO - implement
-		
+	private static char opponentPlayer;
+	public static char getWinner(char[] board, char currentPlayer) {	
 		// Algorithm: 
 		// For each legal move for the current player,
+		if(currentPlayer == TOAD)
+			opponentPlayer = FROG;
+		if(currentPlayer == FROG)
+			opponentPlayer = TOAD;
+		if(currentPlayer == TOAD)
+			for (int i = 0; i < board.length; i++) {
+				if (board[i] == currentPlayer)
+				{
+					if(i <= (board.length - 2) && board[i+1] == EMPTY){
+						board[i + 1] = currentPlayer;
+						board[i] = EMPTY;
+					}
+					else if(i <= (board.length - 3) && board[i+2] == EMPTY){
+						board[i + 2] = currentPlayer;
+						board[i] = EMPTY;
+					}
+					else if((board[i + 1] == opponentPlayer) && board[i + 2] == opponentPlayer){
+						return getWinner(board, opponentPlayer);
+					}
+					else
+						return currentPlayer;
+				}
+			}
+		else
+			for(int i = board.length - 1; i >= 0; i--){
+				if (board[i] == currentPlayer){
+					if(i >= 1 && board[i - 1] == EMPTY){
+						board[i - 1] = currentPlayer;
+						board[i] = EMPTY;
+					}
+					else if(i >= 2 && board[i - 2] == EMPTY){
+						board[i - 2] = currentPlayer;
+						board[i] = EMPTY;
+					}
+					else if((board[i - 1] == opponentPlayer) && board[i - 2] == opponentPlayer){
+						return getWinner(board, opponentPlayer);
+					}
+					else
+						return currentPlayer;
+				}
+			}
+
+		return getWinner(board, opponentPlayer);
+				
 		//   - if we call getWinner on the resulting game state (new board, other player)
 		//     and find that this leads to a win for the current player, 
 		//     then return the current player.
 		// Otherwise, failing to find a winning play (or any legal play), return the opponent player.
-
-		return null;
 	}
 
 }
