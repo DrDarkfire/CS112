7b9217f8ce8b2b7d86e196f1ab7753e832050582
kmurdough <36051815+kmurdough@users.noreply.github.com>
2018-02-12 13:39:49 -0500

    added SuitIsRed to whole thing

--- Cascade.java
+++ Cascade.java
@@ -1,12 +1,14 @@
 
 public class Cascade extends CardStack {
 	
+	public static boolean[] suitIsRed = {false, true, true, false};
+
 	public Cascade() {
 		return;
 	}
 	
 	public boolean playTo(Card card) {
-		if (stack.isEmpty() || (card.suitIsRed != stack.peek().suitIsRed 
+		if (stack.isEmpty() || (suitIsRed[card.getSuit()] != suitIsRed[stack.peek().getSuit()] 
 				&& card.getRank() == (stack.peek().getRank() - 1)))
 			return true;
 		return false;
