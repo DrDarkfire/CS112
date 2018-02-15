0135ccf450b33d2d5a7eb61efcfbf30a14d13e62
DrDarkfire <2017BrandonH@gmail.com>
2018-02-15 18:43:05 -0500

    Fixed toString

--- FreecellGame.java
+++ FreecellGame.java
@@ -1,41 +1,3 @@
-
-public class FreecellGame {
-	
-	private CardStack[] stacks = new CardStack[17];
-	
-    public FreecellGame(long seed) {
-    	stacks[0] = new Deck(seed);
-    	// change this to make specific CardStacks (e.g. Cell)
-    	for (int i = 1; i < stacks.length; ++i)
-    		stacks[i] = new CardStack();
-    	// deal deck into the cascades
-    	int i = 9;
-    	while (!stacks[0].isEmpty()) {
-    		stacks[i++].addCard(stacks[0].removeTopCard());
-    		if (i == stacks.length)
-    			i = 9;
-    	}
-    }
-
-    public boolean play(int srcStackNum, int destStackNum) {
-    	return stacks[destStackNum].playTo(stacks[srcStackNum]);
-    }
-
-    public Card[] getStack(int stackNum) {
-    	return null;
-    }
-
-    public boolean isGameOver() {
-    	return false;
-    }
-
-
-    public java.lang.String toString() {
-    	return null;
-    }
-}
-
-
 import java.util.Arrays;
 
 public class FreecellGame {        
@@ -105,19 +67,18 @@ public class FreecellGame {
 	//    whether or not the game has been won (that is, all cards are on foundations)
 
 	//toString
-
 	public java.lang.String toString(){
 		String output;
-		output = "Cells: /n";
+		output = "Cells: \n";
 		for (int i = 1; i <= 4; i++)
-			output += " " + i + ": " + Arrays.toString(getStack(i)) + "/n";
-		output = "Foundations: /n";
+			output += " " + i + ": " + Arrays.toString(getStack(i)) + "\n";
+		output += "Foundations: \n";
 		for (int j = 5; j <= 8; j++)
-			output += " " + j + ": " + Arrays.toString(getStack(j)) + "/n";
-		output = "Cascades: /n";
-		output = " 9: " + Arrays.toString(getStack(9));
+			output += " " + j + ": " + Arrays.toString(getStack(j)) + "\n";
+		output += "Cascades: \n";
+		output += " 9: " + Arrays.toString(getStack(9)) + "\n";
 		for (int k = 10; k <= 16; k++)
-			output += k + ": " + Arrays.toString(getStack(k)) + "/n";
+			output += k + ": " + Arrays.toString(getStack(k)) + "\n";
 		return output;
 	}
 
