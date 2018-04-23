5faf4a04a461a11d38814a6b284ee1d7b62c88d6
DrDarkfire <2017BrandonH@gmail.com>
2018-04-23 10:50:50 -0400

    Update Item.java

--- InteractiveAdventure/Item.java
+++ InteractiveAdventure/Item.java
@@ -1,1 +1,13 @@
+public class Item {
+	String name, description;
 
+	public Item(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+	
+	public String toString() {
+		return name;
+	}
+	
+}
