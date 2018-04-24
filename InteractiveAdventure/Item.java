552e906b0faca20d64994ed5654f988f208ccdc3
kmurdough <36051815+kmurdough@users.noreply.github.com>
2018-04-23 21:12:10 -0400

    kaylupdate

--- InteractiveAdventure/Item.java
+++ InteractiveAdventure/Item.java
@@ -1,13 +1,17 @@
-public class Item {
-	String name, description;
+import java.util.HashSet;
+import java.util.Set;
 
-	public Item(String name, String description) {
+public class Item {
+	String name, description, gettable;
+	public Set<Object> attributes = new HashSet<>();
+
+	public Item(String name, String description, String gettable) {
 		this.name = name;
 		this.description = description;
+		this.gettable = gettable;
 	}
 	
 	public String toString() {
 		return name;
 	}
-	
 }
