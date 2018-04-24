19a9ba383d9061d322718b66c7fc8dba3509774b
kmurdough <36051815+kmurdough@users.noreply.github.com>
2018-04-23 21:11:49 -0400

    kaylupdate

--- InteractiveAdventure/Location.java
+++ InteractiveAdventure/Location.java
@@ -6,16 +6,17 @@ public class Location {
 
 	public String name;
 	public String description;
-	boolean isVisited = false;
+	public boolean enterable;
+	public boolean isVisited;
 	public Set<Item> items = new HashSet<>();
-	public HashMap<String, Location> exits = new HashMap<String, Location>();
+	public HashMap<String, Location> exits = new HashMap<String, Location>();  //Mapping a key to a value
 
-	public Location(String name, String description) {
+	public Location(String name, String description, boolean enterable) {
 		this.name = name;
 		this.description = description;
+		this.enterable = enterable;
 	}
 
-	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append(name);
@@ -25,13 +26,15 @@ public class Location {
 		}
 		if (!items.isEmpty()) {
 			sb.append("\nYou see:");
-			for (Item item : items)
+			for (Item item : items) {
 				sb.append(" " + item);
+			}
 		}
 		if (!exits.isEmpty()) {
 			sb.append("\nExits:");
-			for (String dir : exits.keySet())
+			for (String dir : exits.keySet()) {
 				sb.append(" " + dir);
+			}
 		}
 		return sb.toString();
 	}
