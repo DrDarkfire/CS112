fdc8353139609aaac6d6941a381e6c320c0b9664
DrDarkfire <2017BrandonH@gmail.com>
2018-09-12 11:51:01 -0400

    Update helloworld.asm

--- CS221/helloworld.asm
+++ CS221/helloworld.asm
@@ -1,5 +1,5 @@
 .text
-  li $sv0, 4
+  li $sv0, 4 load immediate
   la $a0, output
   syscall
   
@@ -7,7 +7,27 @@
   li $a0, 7
   syscall
   
+  li $v0, 4
+  la $a0, prompt
+  syscall
+  
+  li $v0, 8
+  la $a0, buffer
+  li $a1, 80
+  syscall
+  
+  li $v0, 4
+  la $a0, newLine
+  syscall
+  
+  li $v0, 4
+  la $a0, buffer
+  syscall
+  
   li $v0, 10
   syscall
 .data
 output: .asciiz "Hello World\n"
+prompt: .asciiz "Enter a string: "
+buffer: .space 80
+newLine: .asciiz "\n"
