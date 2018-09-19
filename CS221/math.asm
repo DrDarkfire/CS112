e6f6a1c906b553a7423c82629696c183ce948cd9
DrDarkfire <2017BrandonH@gmail.com>
2018-09-19 11:44:46 -0400

    Update math.asm

--- CS221/math.asm
+++ CS221/math.asm
@@ -1,1 +1,14 @@
+.text
+la $a0, a
+li $t2, 4
+addi $t1, $t2, 0xc321 //overflows to negative
+//addi or subi answer, immediate1, immediate2
+move $a0, $t1
+li $v0, 1
+syscall
 
+.li $v0, 10
+syscall
+  
+.data
+.word 0x12345678
