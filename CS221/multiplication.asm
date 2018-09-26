11b34a95ee5ad99a99f329d0caf3f1b83355eb5f
DrDarkfire <2017BrandonH@gmail.com>
2018-09-26 11:47:37 -0400

    Update multiplication.asm

--- CS221/multiplication.asm
+++ CS221/multiplication.asm
@@ -1,1 +1,16 @@
-
+.text
+  li $t0, 10
+  li $t1, 20
+  mult $t0, $t1
+  mflo $t3        move from low
+  li $v0, 10
+  syscall
+  
+  li $t0, 19
+  li $t1, 4
+  div $t1, $t0
+  
+  mflo $t3 would be 4
+  mfhi $t3 would be 3
+  
+  syscall
