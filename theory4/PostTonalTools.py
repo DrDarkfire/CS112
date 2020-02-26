a23bca76cfb24a26e3acfaa8f5c7869354f30d20
Brandon H <2017BrandonH@gmail.com>
2020-02-26 15:24:49 -0500

    Update PostTonalTools.py

--- theory4/PostTonalTools.py
+++ theory4/PostTonalTools.py
@@ -1,1 +1,10 @@
-
+# pitch class list must be in a list form
+def to_normal_form(pitchClassList):
+    # rotate through list clockwise insert will shift everything to the right technically
+    rotations = [pitchClassList]
+    curr = pitchClassList
+    for x in pitchClassList:
+        # take the last postion and place it into first
+        curr.insert(0, curr.pop)
+        # add to rotations list
+        
